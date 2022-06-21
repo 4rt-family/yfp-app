df5d5cda1e4a0d6315572421d0b6bf78a8f2a1ae
vanvilecks <vanvilecks@gmail.com>
2022-06-22 01:21:54 +0700

    chore: fix position line import

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import PropTypes from "prop-types";
-import React from "react";
-import { usePromiseTracker } from "react-promise-tracker";
+import Image from 'next/image';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { usePromiseTracker } from 'react-promise-tracker';
 
-import AddSVG from "./images/add.url.svg";
-import RefreshSVG from "./images/refresh.url.svg";
-import Logo from "./images/logo.url.svg";
+import AddSVG from './images/add.url.svg';
+import Logo from './images/logo.url.svg';
+import RefreshSVG from './images/refresh.url.svg';
 
 const Header = ({ setRandomPost }) => {
   const { promiseInProgress } = usePromiseTracker();
@@ -15,17 +15,21 @@ const Header = ({ setRandomPost }) => {
     <header className="container pt-18 pb-5 text-center">
       <Image className="mx-auto" src={Logo} width={298} height={135} alt="logo" />
 
-      <div className="pt-10 grid grid-cols-3 xs:grid-cols-2">
-        <div className="my-auto col-start-2 xs:col-start-1">
+      <div className="grid grid-cols-3 pt-10 xs:grid-cols-2">
+        <div className="col-start-2 my-auto xs:col-start-1">
           <button type="button" onClick={setRandomPost}>
-            <Image className={promiseInProgress ? "spinner-border animate-spin" : null} src={RefreshSVG} width={30}
-                   height={30} />
-            <span className="text-10 font-mono ml-4 align-super uppercase">Другая паста</span>
+            <Image
+              className={promiseInProgress ? 'spinner-border animate-spin' : null}
+              src={RefreshSVG}
+              width={30}
+              height={30}
+            />
+            <span className="text-10 ml-4 align-super font-mono uppercase">Другая паста</span>
           </button>
         </div>
         <div className="ml-auto">
           <Link href="/add_article">
-            <Image className="cursor-pointer" src={AddSVG} width={70} height={70}></Image>
+            <Image className="cursor-pointer" src={AddSVG} width={70} height={70} />
           </Link>
         </div>
       </div>
@@ -34,7 +38,7 @@ const Header = ({ setRandomPost }) => {
 };
 
 Header.propTypes = {
-  setRandomPost: PropTypes.func.isRequired
+  setRandomPost: PropTypes.func.isRequired,
 };
 
 export default Header;
