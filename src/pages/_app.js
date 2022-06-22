9e55cc581af41cc1f7665ca54e5dba8adb74c020
vanvilecks <vanvilecks@gmail.com>
2022-06-23 06:51:12 +0700

    chore: remove comment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,6 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     </Head>
-    // eslint-disable-next-line react/jsx-filename-extension
     <Component {...pageProps} />
   </>
 );
