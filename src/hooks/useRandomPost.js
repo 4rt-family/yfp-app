ea0406387a4279eb2688b639885e6df2d154bcb9
vanvilecks <vanvilecks@gmail.com>
2022-06-23 06:41:58 +0700

    fix: change get to post in useRandomPost

--- src/hooks/useRandomPost.js
+++ src/hooks/useRandomPost.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import { trackPromise } from "react-promise-tracker";
+import { useState } from 'react';
+import { trackPromise } from 'react-promise-tracker';
 
 function useRandomPost() {
   const [post, setPost] = useState({
-    article: "",
-    title: "",
-    tags: []
+    article: '',
+    title: '',
+    tags: [],
   });
 
-  const getRandomPost = async () => {
-    const request = await trackPromise(fetch("/api/pasta"));
-    const { data } = await request.json();
+  const setRandomPost = async () => {
+    const request = await trackPromise(fetch('/api/pasta', { method: 'POST' }));
+    const {
+      data: { Title, Content, Tags },
+    } = await request.json();
 
-    return {
-      title: data.Title,
-      article: data.Content,
-      tags: data.Tags ? data.Tags : []
-    };
+    setPost({
+      title: Title,
+      article: Content,
+      tags: Tags || [],
+    });
   };
-
-  const setRandomPost = async () => setPost(await getRandomPost());
 
   return [post, setRandomPost];
 }
